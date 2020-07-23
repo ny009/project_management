b2ff47b3eedbf6a1e3d9ec85100954f990be55eb
lvpeiwen927 <294150983@qq.com>
2020-07-23 11:48:39 +0800

    Add files via upload

---
+++ src/views/notifications/Createannouncement.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <CRow>
+      <CCol>
+        <CCard>
+          <CCardHeader>
+            <strong>Create An Announcement</strong>
+          </CCardHeader>
+          <CCardBody>
+            <CForm>
+              <CInput
+                label="Announcement Title"
+                horizontal
+              />
+              <CTextarea
+                label="Announcement Content"
+                horizontal
+                rows="20"
+              />
+            </CForm>
+          </CCardBody>
+          <CCardFooter>
+            <CButton type="submit" size="sm" color="primary"><CIcon name="cil-check-circle"/> Submit</CButton>
+            <CButton type="reset" size="sm" color="danger"><CIcon name="cil-ban"/> Reset</CButton>
+          </CCardFooter>
+        </CCard>
+      </CCol>
+    </CRow>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Forms',
+  data () {
+    return {
+      selected: [], // Must be an array reference!
+      show: true,
+      horizontal: { label:'col-3', input:'col-9' },
+      options: ['Option 1', 'Option 2', 'Option 3'],
+      selectOptions: [
+        'Option 1', 'Option 2', 'Option 3',
+        { 
+          value: 'some value', 
+          label: 'Selected option'
+        }
+      ],
+      selectedOption: 'some value',
+
+      formCollapsed: true,
+      checkboxNames: ['Checkboxes', 'Inline Checkboxes',
+                      'Checkboxes - custom', 'Inline Checkboxes - custom'],
+      radioNames: ['Radios', 'Inline Radios',
+                    'Radios - custom', 'Inline Radios - custom']
+    }
+  },
+  methods: {
+    validator (val) {
+      return val ? val.length >= 4 : false
+    }
+  }
+}
+</script>
