5f18bcc91dca760fb12bb25d30a45a0f7c120f02
lvpeiwen927 <294150983@qq.com>
2020-07-27 22:34:42 +0800

    Add files via upload
    添加了所有项目展示 但是样式仍然需要改进 页面跳转传递p_id没有写

--- src/views/project/Allproject.vue
+++ src/views/project/Allproject.vue
@@ -12,11 +12,10 @@
         </CCard>
         </CCol>
     </CRow>
-    <CRow>
-        <CCard>
+    <CRow  v-for="p in project_info">
+        <CCard >
           <CCardHeader>
-
-            <strong>{{projectname}}</strong>
+            <strong>{{p.title}}</strong>
             <div class="card-header-actions">
               <a 
                 href="/#/team/allteam" 
@@ -26,108 +25,17 @@
             </div>
           </CCardHeader>
           <CCardBody>
-            {{loremIpsum}}
+            {{p.outline}}
           </CCardBody>
-                    <CCardfooter>
-          Start time: 2020-06-01
-          End Time: 2020-07-14
-          </CCardfooter>
         </CCard>
     </CRow>
-        <CRow>
-        <CCard>
-          <CCardHeader>
-
-            <strong>{{projectname}}</strong>
-            <div class="card-header-actions">
-              <a 
-                href="/" 
-                target="_blank"
-              >
-                <CButton color="primary" size="sm">Enroll</CButton>
-              </a>
-            </div>
-          </CCardHeader>
-          <CCardBody>
-            {{loremIpsum}}
-          </CCardBody>
-                    <CCardfooter>
-          Start time: 2020-06-01
-          End Time: 2020-07-14
-          </CCardfooter>
-        </CCard>
-    </CRow>    <CRow>
-        <CCard>
-          <CCardHeader>
-
-            <strong>{{projectname}}</strong>
-            <div class="card-header-actions">
-              <a 
-                href="/" 
-                target="_blank"
-              >
-                <CButton color="primary" size="sm">Enroll</CButton>
-              </a>
-            </div>
-          </CCardHeader>
-          <CCardBody>
-            {{loremIpsum}}
-          </CCardBody>
-                    <CCardfooter>
-          Start time: 2020-06-01
-          End Time: 2020-07-14
-          </CCardfooter>
-        </CCard>
-    </CRow>    <CRow>
-        <CCard>
-          <CCardHeader>
-
-            <strong>{{projectname}}</strong>
-            <div class="card-header-actions">
-              <a 
-                href="/" 
-                target="_blank"
-              >
-                <CButton color="primary" size="sm">Enroll</CButton>
-              </a>
-            </div>
-          </CCardHeader>
-          <CCardBody>
-            {{loremIpsum}}
-          </CCardBody>
-          <CCardfooter>
-          Start time: 2020-06-01
-          End Time: 2020-07-14
-          </CCardfooter>
-        </CCard>
-    </CRow>    <CRow>
-        <CCard>
-          <CCardHeader>
-
-            <strong>{{projectname}}</strong>
-            <div class="card-header-actions">
-              <a 
-                href="/" 
-                target="_blank"
-              >
-                <CButton color="primary" size="sm">Enroll</CButton>
-              </a>
-            </div>
-          </CCardHeader>
-          <CCardBody>
-            {{loremIpsum}}
-          </CCardBody>
-                    <CCardfooter>
-          Start time: 2020-06-01
-          End Time: 2020-07-14
-          </CCardfooter>
-        </CCard>
-    </CRow>
+        
  
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'Cards',
@@ -136,8 +44,17 @@ export default {
       show: true,
       isCollapsed: true,
       loremIpsum: 'Project-based learning (e.g., programming projects, design projects, capstone projects) is common in education. This includes promoting students’ interaction with “real-life” projects and professional communities through support tools (e.g., questions and answers platforms such as Stack Overflow programming community, open source projects involving professionals and volunteers). In general, students rely on instructors, peers and mentors for feedbacks on their learning projects. While there is plethora of tools (e.g., task management and collaboration platforms like Trello, Asana, Jira and other) to support learners with task management, these tools are not designed with student projects in mind. Especially in a situation where remote work is a reality and brings its own challenges, it is important to provide appropriate support for project-based learning given that project members are used to frequent face to face meetings to handle project matters. ',
-      projectname:'Project-based Learning Dojo '
+      projectname:'Project-based Learning Dojo ',
+      project_info: ""
     }
-  }
+  },
+  created() {
+      var that = this;
+      axios.get("https://project-dojo.herokuapp.com/api/v2/project").then(response => {
+      var res = response.data;
+      console.log(res);
+      that.project_info = res;
+    });
+  },
 }
 </script>
