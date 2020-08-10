5326053f2a1283d70da510d514624dee941ac458
lvpeiwen927 <294150983@qq.com>
2020-08-10 23:14:14 +0800

    Add files via upload

--- src/views/project/Allproject.vue
+++ src/views/project/Allproject.vue
@@ -15,7 +15,7 @@
             <br>
             <strong>End Date:</strong> {{p.end_time}}
             <div style="float:right">
-                <CButton color="primary" size="sm" @click="enrollproject(p.id)">Enroll</CButton>
+                <CButton :color="getBadge(p.status)" size="sm" @click="enrollproject(p.id)">{{p.status}}</CButton>
             </div>
           </CCardFooter>
         </CCard>
@@ -36,6 +36,14 @@ export default {
       project_info: ""
     }
   },
+  created() {
+      var that = this;
+      axios.get("http://127.0.0.1:5000/project/all?user_id="+localStorage.uid).then(response => {
+      var res = response.data;
+      console.log(res);
+      that.project_info = res;
+    });
+  },
   methods:{
     enrollproject:function(project_id){
       //alert(project_id);
@@ -46,28 +54,28 @@ export default {
       },{emulateJSON:true})
       .then(function(response){
         if(response.data.status === 'SUCCESS'){
-          alert('enroll success');
+          alert('enroll success! You can join a team now.');
           self.$router.push('../team/Allteam?project_id='+project_id);
         }else{
-          alert(response.data.status);
-          self.$router.push('../team/Allteam?project_id='+project_id);
+          alert("You have already enrolled!\n If you want to join a team.\n Please go to My project.");
+          //self.$router.push('../team/Allteam?project_id='+project_id);
         }
           
       },function(error){
         alert('fail');
+        })
+    },
+    getBadge (status) {
+      switch (status) {
+        case 'Enroll': return 'success'
+        case 'Already Enrolled': return 'secondary'
+        case 'Pending': return 'warning'
+        case 'Banned': return 'danger'
+        default: 'primary'
       }
-      
-      )
-    }
-    
+    },
   },
-  created() {
-      var that = this;
-      axios.get("http://127.0.0.1:5000/project/all").then(response => {
-      var res = response.data;
-      console.log(res);
-      that.project_info = res;
-    });
-  },
+  
+  
 }
 </script>
