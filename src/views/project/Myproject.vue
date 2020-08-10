03dcf06f7d74036752ded29d8601f939af9a2b31
lvpeiwen927 <294150983@qq.com>
2020-08-10 23:49:07 +0800

    Add files via upload

--- src/views/project/Myproject.vue
+++ src/views/project/Myproject.vue
@@ -1,10 +1,10 @@
 <template>
   <CRow>
-      <CCol sm="6" lg="3" v-for="p in project_info">
-      <a href="/#/team/myteam?" >
+      <CCol sm="6" lg="3" v-for="(p,index) in project_info">
+      <a :href="getGoodsHref(index )" >
         <CWidgetProgress
           :header="p.name"
-          :text="p.course"
+          :text="p.status" 
           :footer="p.end_time"
           color="gradient-success"
           inverse :value="70"
@@ -21,16 +21,26 @@ export default {
   name: 'project',
   data: function () {
     return {
-      project_info: ""
+      project_info: []
     }
   },
   created() {
       var that = this;
-      axios.get("http://127.0.0.1:5000/project/all?id="+localStorage.uid).then(response => {
+      axios.get("http://127.0.0.1:5000/project/my?user_id="+localStorage.uid).then(response => {
       var res = response.data;
       console.log(res);
       that.project_info = res;
     });
   },
+  methods:{
+    getGoodsHref:function(val){
+      if(this.project_info[val].status == "No team"){
+        return '/#/team/allteam?project_id='+this.project_info[val].id;
+      } else{
+        return '/#/team/myteam?project_id='+this.project_info[val].id;
+      }
+	    
+	}
+  }
 }
 </script>
