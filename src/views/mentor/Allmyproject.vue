6d9debdb38d31eb943025f0488444e2a944dd11b
lvpeiwen927 <294150983@qq.com>
2020-08-12 00:22:12 +0800

    Add files via upload

--- src/views/mentor/Allmyproject.vue
+++ src/views/mentor/Allmyproject.vue
@@ -17,13 +17,13 @@
           <CCollapse :show="collapse" :duration="400">
             <CCard body-wrapper>
               <CForm>
-                <CInput placeholder="Title" label="Title" horizontal/>
-                <CInput placeholder="Detail" label="Detail" horizontal/>
-                <CInput placeholder="Term" label="Term" horizontal/>
-                <CInput placeholder="Course" label="Course" horizontal/>
-                <CInput type="date"  label="Start_time" horizontal/>
-                <CInput type="date"  label="End_time" horizontal/>
-                <CButton type="submit" size="sm" color="primary" @click=""><CIcon name="cil-check-circle"/> Submit</CButton>
+                <CInput placeholder="Title" label="Title" horizontal v-model="newproject.name"/>
+                <CTextarea placeholder="Detail" label="Detail" horizontal v-model="newproject.detail"/>
+                <CSelect placeholder="Term" label="Term" horizontal :options="options" @change="changeTerm($event)"/>
+                <CInput placeholder="Course" label="Course" horizontal v-model="newproject.course"/>
+                <CInput type="date"  label="Start_time" horizontal v-model="newproject.start_time"/>
+                <CInput type="date"  label="End_time" horizontal v-model="newproject.end_time"/>
+                <CButton type="submit" size="sm" color="primary" @click="createproject()"><CIcon name="cil-check-circle"/> Submit</CButton>
               </CForm>
             </CCard>
           </CCollapse>
@@ -61,22 +61,60 @@
 
 <script>
 import axios from 'axios'
-
 export default {
   name: 'Cards',
   data: function () {
     return {
       collapse: false,
-      project_info:[]
+      options:[{value:"20T1", label:"20T1"}, {value:"20T2", label:"20T2"}, {value:"20T3", label:"20T3"}],
+      project_info:[],
+      newproject:{
+        name:'',
+        detail:'',
+        term:'',
+        course:'',
+        start_time:'',
+        end_time:'',
+      }
     }
   },
   created() {
       var that = this;
-      axios.get("http://127.0.0.1:5000/project/all").then(response => {
-      var res = response.data;
-      console.log(res);
-      that.project_info = res;
+      //alert(localStorage.uid);
+      axios.get("http://127.0.0.1:5000/project/mentor?user_id="+localStorage.uid).then(response => {
+        var res = response.data;
+        console.log(res);
+        that.project_info = res;
     });
   },
+  methods:{
+    changeTerm(event) {
+      this.newproject.term = event.target.value; 
+      console.log("choose ",this.newproject.term)
+    },
+    createproject:function () {
+      var self = this;
+      axios.post("http://127.0.0.1:5000/project/create",{
+        name:self.newproject.name,
+        detail:self.newproject.detail,
+        term:self.newproject.term,
+        course:self.newproject.course,
+        mentor_id:localStorage.uid,
+        start_time:self.newproject.start_time,
+        end_time:self.newproject.end_time
+        
+      },{emulateJSON:true})
+      .then(function(response){
+        if(response.data.status === 'SUCCESS'){
+          alert('SUCCESS');
+          location.reload();
+        }else{
+          alert('FAIL');
+        }
+      },function(error){
+          alert('FAIL');
+      })
+    },
+  }
 }
 </script>
